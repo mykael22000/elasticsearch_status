27f98ea0fff28c07c5b5dcf031023c60a952364d
mykael22000 <mikclrk@gmail.com>
2017-09-18 23:34:53 +0800

    Support for Kibana 5.6.1
    Modified to work with Kibana 5.6.1 - CallWithRequest, import { uiModule }, Kibana version.

---
+++ app.js
@@ -0,0 +1,35 @@
+import { uiModules } from 'ui/modules';
+import uiRoutes from 'ui/routes';
+
+import 'ui/autoload/styles';
+import './less/main.less';
+import overviewTemplate from './templates/index.html';
+import detailTemplate from './templates/detail.html';
+
+uiRoutes.enable();
+uiRoutes
+.when('/', {
+  template: overviewTemplate,
+  controller: 'elasticsearchStatusController',
+  controllerAs: 'ctrl'
+})
+.when('/index/:name', {
+  template: detailTemplate,
+  controller: 'elasticsearchDetailController',
+  controllerAs: 'ctrl'
+});
+
+uiModules
+.get('app/elasticsearch_status')
+.controller('elasticsearchStatusController', function ($http) {
+  $http.get('../api/elasticsearch_status/indices').then((response) => {
+    this.indices = response.data;
+  });
+})
+.controller('elasticsearchDetailController', function($routeParams, $http) {
+  this.index = $routeParams.name;
+
+  $http.get(`../api/elasticsearch_status/index/${this.index}`).then((response) => {
+    this.status = response.data;
+  });
+});
